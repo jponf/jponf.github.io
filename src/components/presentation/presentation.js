8c09bb06f1a8df5a5b3d7ab8bb8d5aeb2a5c9eb8
Josep Pon Farreny <jpon@ilerfred.com>
2021-05-18 09:59:09 +0200

    Static content generated at build time

--- src/components/presentation/presentation.js
+++ src/components/presentation/presentation.js
@@ -1,22 +1,28 @@
 import waitForElement from '@/utils.js';
 
-import presentation from "./presentation.html"
+// import presentation from "./presentation.html"
 import './presentation.css'
 
-export default function renderPresentation(container) {
-    var template = document.createElement('template');
-    template.innerHTML = presentation;
 
-    if (template.content.childNodes.length != 1) {
-        document.write("Error loading presentation HTML")
-    } else {
-        container.appendChild(template.content.childNodes[0]);
-        waitForElement("#presentation").then(function() {
-            var navbar = document.getElementById("main-navbar");
-            var presentation = document.getElementById("presentation");
+// export function renderPresentation(container) {
+//     var template = document.createElement('template');
+//     template.innerHTML = presentation;
 
-            // Account for navigation var height
-            presentation.style.paddingTop = navbar.offsetHeight + "px";
-        });
-    }
+//     if (template.content.childNodes.length != 1) {
+//         document.write("Error loading presentation HTML")
+//     } else {
+//         container.appendChild(template.content.childNodes[0]);
+//         setUpPresentation();
+//     }
+// }
+
+
+export function setUpPresentation() {
+    waitForElement("#presentation").then(function() {
+        var navbar = document.getElementById("main-navbar");
+        var presentation = document.getElementById("presentation");
+
+        // Account for navigation var height
+        presentation.style.paddingTop = navbar.offsetHeight + "px";
+    });
 }
